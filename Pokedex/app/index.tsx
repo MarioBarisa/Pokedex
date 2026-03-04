cf8f9b476b6af5eb1dafe77ffd5f037f5e0464ba
Mario Barisa <mario@barisa.me>
2026-03-04 21:15:26 +0100

    osnovna lista pokemon imena

--- Pokedex/app/index.tsx
+++ Pokedex/app/index.tsx
@@ -1,15 +1,36 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, Text, View } from "react-native";
 
 export default function Index() {
+
+  const [pokemons, setPokemons] = useState([])
+
+
+useEffect(() => {
+  const fetchPokemon = async () => {
+    try {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=40");
+      const data = await response.json();
+      setPokemons(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+ fetchPokemon();
+}, [])
+
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ScrollView>
+
+      <Text>Pokedex</Text>
+
+      {pokemons.map((pokemon) => (
+        <View key={pokemon.name}>
+          {pokemon.name}
+          </View>
+        ))}
+
+    </ScrollView>
   );
 }
